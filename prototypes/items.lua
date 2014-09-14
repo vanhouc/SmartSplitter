4ba4ec1d773d8bb5507312e05bb9d91e3e8246a0
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-09-14 13:29:36 -0400

    added items.lua

---
+++ prototypes/items.lua
@@ -0,0 +1,12 @@
+data: extend ({
+
+	{
+		type = "item",
+		name = "smartsplitter",
+		icon = "__SmartSplitter__/graphics/icon_smartsplitter.png",
+		flags = { "goes-to-quickbar" },
+		subgroup = "belt",
+		place_result="smartsplitter",
+		stack_size= 50,
+	}
+})
